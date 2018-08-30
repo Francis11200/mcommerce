2c485e5ddd285b9592e19c65dfcb06926eca28c4
DESCHAMPS <francisdeschamps@free.fr>
2018-08-30 17:10:51 +0200

    MAJ 30-08-18

--- microservice-paiement/src/main/java/com/mpaiement/MpaiementApplication.java
+++ microservice-paiement/src/main/java/com/mpaiement/MpaiementApplication.java
@@ -1,9 +1,13 @@
 package com.mpaiement;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("com.mpaiement")
 public class MpaiementApplication {
 
 	public static void main(String[] args) {
